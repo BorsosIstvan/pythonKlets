158bc49a4b179eda842aeb6cc6bb61d45c99bac4
BorsosIstvan <bistvanus@gmail.com>
2024-04-05 19:07:12 +0200

    Hoi

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, jsonify
+import time
+
+app = Flask(__name__)
+
+# Route voor de indexpagina
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Route om elke 30 seconden een bericht te verzenden
+@app.route('/message')
+def send_message():
+    # Stuur een tijdelijke bericht
+    message = "Dit is een testbericht van de server."
+    return jsonify({'message': message})
+
+if __name__ == '__main__':
+    app.run(debug=True)
