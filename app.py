67aa6884bb8892d1b96b0d6ba0f9efb047ee85ff
BorsosIstvan <bistvanus@gmail.com>
2024-04-05 20:25:19 +0200

    met socketio

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 import time
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
+# Indexpagina
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/send_message', methods=['POST'])
-def send_message():
-    data = request.get_json()
-    message = data.get('message', '')
-    reversed_message = message[::-1]  # Keer het bericht om
-    time.sleep(1)  # Wacht 10 seconden
-    return jsonify({'reversed_message': reversed_message})
+# Functie voor het periodiek verzenden van berichten naar de client
+def send_messages():
+    count = 0
+    while True:
+        socketio.emit('server_message', {'message': f"Dit is bericht {count}"})
+        count += 1
+        time.sleep(10)  # Wacht 10 seconden tussen elk bericht
+
+# Start het verzenden van berichten wanneer de server wordt gestart
+@socketio.on('connect')
+def handle_connect():
+    send_messages()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    socketio.run(app, host='0.0.0.0', port=8000)
