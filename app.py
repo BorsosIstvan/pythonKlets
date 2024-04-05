fd220539cf3e8837d0f5b08da64f4faa405d73c5
BorsosIstvan <bistvanus@gmail.com>
2024-04-05 19:50:52 +0200

    sturen en ontvangen

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def send_message():
     data = request.get_json()
     message = data.get('message', '')
     reversed_message = message[::-1]  # Keer het bericht om
-    time.sleep(10)  # Wacht 10 seconden
+    time.sleep(1)  # Wacht 10 seconden
     return jsonify({'reversed_message': reversed_message})
 
 if __name__ == '__main__':
